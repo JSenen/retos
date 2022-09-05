75c02c094217d725d6abd8791eb5da8a53c6c138
Juan Senen <juansenen@hotmail.com>
2022-09-05 19:40:18 +0200

    Change Chalenge9

--- src/main/retos2022/java/Chalenge9.java
+++ src/main/retos2022/java/Chalenge9.java
@@ -10,43 +10,143 @@
  *
  */
 
+
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Chalenge9 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        System.out.println("*** MENU ***");
-        System.out.println("1) Alphabet to Morse");
-        System.out.println("2) Morse to Alphabet");
-        System.out.println("OPTION: ");
-        String option = console.nextLine();
-        String phrase;
-        switch (option){
-            case("1"):
-                System.out.print("Intro phrase: ");
-                phrase = console.nextLine();
-                alphaToMorse(phrase.toUpperCase());
-                System.out.println(phrase);
-                break;
-            case("2"):
-                System.out.print("Intro morse: ");
-                phrase = console.nextLine();
-                morseToAlpha(phrase);
-                break;
+        System.out.println("INTRO PHRASE (MORSE OR ALPHABET) : ");
+        String phrase = console.nextLine();
+        boolean alphabetic;
+        if (phrase.toUpperCase().matches("[a-zA-Z_0-9\\s]*")){ /* REGEX permite conocer si texto es Alfabetico o Morse */
+            System.out.println("IN NEXT LINE YOU HAVE THE MORSE PHRASE");
+            alphaToMorse(phrase);
+        }else {
+            System.out.println("INT NEXT LINE YOU HAVE ALPHABETIC PHRASE");
+            morseToAlpha(phrase);
         }
+
+
+
     }
-    public static ArrayList<String> alphaToMorse(String phrase){
-        ArrayList<String> alphaphrase = new ArrayList<>();
-        for (int i = 0; i <= phrase.length(); i++){
+    public static void alphaToMorse(String phrase){
+        int longphrase =(phrase.length())-1;
+        ArrayList<String> changePhrase = new ArrayList<>();
+        for (int i = 0; i <= longphrase; i++){
             String word = String.valueOf(phrase.charAt(i));
             if(word.equalsIgnoreCase("A")){
-                alphaphrase.add(i,word);
+                changePhrase.add(".-");
+            } else if (word.equalsIgnoreCase("B")) {
+                changePhrase.add("-...");
+            }else if (word.equalsIgnoreCase("C")) {
+                changePhrase.add("-.-.");
+            }else if (word.equalsIgnoreCase("D")) {
+                changePhrase.add("-..");
+            }else if (word.equalsIgnoreCase("E")) {
+                changePhrase.add(".");
+            }else if (word.equalsIgnoreCase("F")) {
+                changePhrase.add("..-.");
+            }else if (word.equalsIgnoreCase("G")) {
+                changePhrase.add("--.");
+            }else if (word.equalsIgnoreCase("H")) {
+                changePhrase.add("....");
+            }else if (word.equalsIgnoreCase("I")) {
+                changePhrase.add("..");
+            }else if (word.equalsIgnoreCase("J")) {
+                changePhrase.add(".---");
+            }else if (word.equalsIgnoreCase("K")) {
+                changePhrase.add("-.-");
+            }else if (word.equalsIgnoreCase("L")) {
+                changePhrase.add("-.--");
+            }else if (word.equalsIgnoreCase("M")) {
+                changePhrase.add("--");
+            }else if (word.equalsIgnoreCase("N")) {
+                changePhrase.add("-.");
+            }else if (word.equalsIgnoreCase("Ñ")) {
+                changePhrase.add("--.--");
+            }else if (word.equalsIgnoreCase("O")) {
+                changePhrase.add("---");
+            }else if (word.equalsIgnoreCase("P")) {
+                changePhrase.add(".--.");
+            }else if (word.equalsIgnoreCase("Q")) {
+                changePhrase.add("--.-");
+            }else if (word.equalsIgnoreCase("R")) {
+                changePhrase.add("-.-");
+            }else if (word.equalsIgnoreCase("S")) {
+                changePhrase.add("...");
+            }else if (word.equalsIgnoreCase("T")) {
+                changePhrase.add("-");
+            }else if (word.equalsIgnoreCase("U")) {
+                changePhrase.add("..-");
+            }else if (word.equalsIgnoreCase("V")) {
+                changePhrase.add("...-");
+            }else if (word.equalsIgnoreCase("W")) {
+                changePhrase.add(".--");
+            }else if (word.equalsIgnoreCase("X")) {
+                changePhrase.add("-..-");
+            }else if (word.equalsIgnoreCase("Y")) {
+                changePhrase.add("-.--");
+            }else if (word.equalsIgnoreCase("Z")) {
+                changePhrase.add("--.");
+            }else if (word.equalsIgnoreCase("0")) {
+                changePhrase.add("-----");
+            }else if (word.equalsIgnoreCase("1")) {
+                changePhrase.add(".----");
+            }else if (word.equalsIgnoreCase("2")) {
+                changePhrase.add("..---");
+            }else if (word.equalsIgnoreCase("3")) {
+                changePhrase.add("...--");
+            }else if (word.equalsIgnoreCase("4")) {
+                changePhrase.add("....-");
+            }else if (word.equalsIgnoreCase("5")) {
+                changePhrase.add(".....");
+            }else if (word.equalsIgnoreCase("6")) {
+                changePhrase.add("-....");
+            }else if (word.equalsIgnoreCase("7")) {
+                changePhrase.add("--...");
+            }else if (word.equalsIgnoreCase("8")) {
+                changePhrase.add("---..");
+            }else if (word.equalsIgnoreCase("9")) {
+                changePhrase.add("----.");
+            }else if (word.equalsIgnoreCase(".")) {
+                changePhrase.add(".-.-.-");
+            }else if (word.equalsIgnoreCase(",")) {
+                changePhrase.add("--..--");
+            }else if (word.equalsIgnoreCase("?")) {
+                changePhrase.add("..--..");
+            }else if (word.equalsIgnoreCase("\"")) {
+                changePhrase.add(".-..-.");
+            }else if (word.equalsIgnoreCase("/")) {
+                changePhrase.add("-..-.");
+            }else {
+                changePhrase.add("  ");
             }
         }
-        return alphaphrase;
+        for (int i=0; i <=longphrase; i++){
+            System.out.print(changePhrase.get(i)+" ");
+        }
     }
-    public static String morseToAlpha(String phrase){
-        return phrase;
+    public static void morseToAlpha(String phrase){
+        int longPhrase = (phrase.length())-1;
+        ArrayList<String> phrase2 = new ArrayList<>();
+        String word = phrase.toUpperCase();
+        String complet = "";
+        for (int i=0; i <= longPhrase; i++){
+            String letter = String.valueOf(word.charAt(i));
+            if (letter.equalsIgnoreCase(".")){
+                complet = complet+letter;
+            }else{
+                phrase2.add(complet);
+            }
+            complet = "";
+
+        }
+        System.out.println(phrase2);
     }
+
+
+
 }
