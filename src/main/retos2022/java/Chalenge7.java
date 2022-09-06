2b74700bc5df688e66ae1f8e10e90c940ebc3a94
Juan Senen <juansenen@hotmail.com>
2022-09-06 17:40:10 +0200

    Finish Chalenge7

--- src/main/retos2022/java/Chalenge7.java
+++ src/main/retos2022/java/Chalenge7.java
@@ -11,5 +11,35 @@
  * - No se pueden utilizar funciones propias del lenguaje que lo resuelvan automáticamente.
  */
 
+
+import java.util.Scanner;
+
 public class Chalenge7 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("INTRO PHRASE: ");
+        String str = console.nextLine();
+        str = str.toUpperCase();
+        System.out.println("INTRO WORD TO COUNT: ");
+        String wrd = console.nextLine();
+        wrd = wrd.toUpperCase();
+
+        int times = 0;
+
+        /** El método startsWith() compara si la cadena comienza con los caracteres de la cadena dada.
+         * Para i = 0 el método str.substring(0) obtiene una subcadena en el índice 0 y comprueba si startsWith(wrd)
+         *  y si coincide, incrementará times en 1; de lo contrario, no aumentará.
+         */
+
+        for (int i=0; i<=str.length(); i++){
+            if (str.substring(i).startsWith(wrd)){
+                times ++;
+            }
+        }
+        System.out.println("Word "+wrd+" it's "+times+" times in the phrase");
+
+
+
+    }
 }
